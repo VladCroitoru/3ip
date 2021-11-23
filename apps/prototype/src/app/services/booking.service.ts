4a1a223d866f16e6b1f8450f4756c9292bf8e1d2
Vlad Croitoru <0croitoruvlad@gmail.com>
2021-11-23 18:58:01 +0100

    feat: udpate dummy services

--- apps/prototype/src/app/services/booking.service.ts
+++ apps/prototype/src/app/services/booking.service.ts
@@ -1,23 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Booking } from '../interfaces/booking.interface';
+import { BOOKINGS } from '../data/bookings';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookingService {
-  private readonly bookings: Booking[] = [
-    {
-      id: 'c854a9c2-0158-40a4-8189-dd160ccbc087',
-      email: 'volunteer@greenkayak.org',
-      date: new Date(),
-      seats: 2,
-      location: {
-        name: 'LETZ SUSHI Islands Brygge',
-        address: 'Snorresgade 1, 2300 København S',
-        description: `Take one for the team and for the sea in the GreenKayak that LETZ SUSHI host at Islandsbrygge. At LETZ SUSHI Islands Brygge you will be given everything you need for a pleasant trip on the water. The staff at the restaurant will point you to the GreenKayak. Then you are ready to go GreenKayaking in the city's beautiful harbor while collecting trash on the water. It is completely free and you can subsequently sort the rubbish into containers. It is community spirit at its best.`,
-      },
-    },
-  ];
+  private bookings = BOOKINGS;
 
   getBooking(bookingId: string): Promise<any> {
     return new Promise((resolve, reject) => {
--- apps/prototype/src/app/services/leaderboard.service.ts
+++ apps/prototype/src/app/services/leaderboard.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { ITINERARIES } from '../data/itineraries';
+import { Itinerary } from '../interfaces/itinerary.interface';
+import {
+  LeaderboardParameter,
+  LeaderboardPeriod,
+} from '../pages/leaderboard/leaderboard.types';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LeaderboardService {
+  itineraries = ITINERARIES;
 
-  constructor() { }
+  getItineraries(
+    period: LeaderboardPeriod,
+    parameter: LeaderboardParameter
+  ): Promise<Itinerary[]> {
+    return new Promise((resolve, reject) => {
+      console.log(parameter);
+      if (parameter === LeaderboardParameter.GARBAGE_WEIGHT) {
+        console.log('yo its garbage time');
+        resolve(this.itineraries.sort(this.sortByProperty('garbageWeight')));
+      }
+      if (parameter === LeaderboardParameter.DISTANCE_ON_WATER) {
+        console.log('yo its distance time');
+        resolve(this.itineraries.sort(this.sortByProperty('distance')));
+      }
+      if (parameter === LeaderboardParameter.TIME_ON_WATER) {
+        console.log('yo it time time');
+        resolve(this.itineraries.sort(this.sortByProperty('duration')));
+      }
+      reject(new Error('Invalid parameter'));
+    });
+  }
+
+  private sortByProperty(property: 'garbageWeight' | 'duration' | 'distance') {
+    return (itinerary1: Itinerary, itinerary2: Itinerary) => {
+      if (itinerary1[property] < itinerary2[property]) {
+        return 1;
+      }
+      if (itinerary1[property] > itinerary2[property]) {
+        return -1;
+      }
+      return 0;
+    };
+  }
 }
